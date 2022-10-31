e23cddfc882b2f0fc75a7de9a9af4a08b5d56151
HolyWatter <qktjdtn97@icloud.com>
2022-10-31 20:01:04 +0900

    styled-reset추가 및 주석제거

--- src/Router.js
+++ src/Router.js
@@ -1,33 +1,33 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Footer from "./components/Footer/Footer";
-// import Nav from "./components/Nav/Nav";
-// import ProductDetail from "./pages/ProductDetail/ProductDetail";
-// import ProductList from "./pages/ProductList/ProductList";
-// import SignUp from "./pages/SignUp/SignUp";
-// import Login from "./pages/Login/Login";
-// import Buy from "./pages/BuySell/Buy/Buy";
-// import Sell from "./pages/BuySell/Sell/Sell";
-// import StylePage from "./pages/Style/StylePage";
+import Footer from "./components/Footer/Footer";
+import Nav from "./components/Nav/Nav";
+import ProductDetail from "./pages/ProductDetail/ProductDetail";
+import ProductList from "./pages/ProductList/ProductList";
+import SignUp from "./pages/SignUp/SignUp";
+import Login from "./pages/Login/Login";
+import Buy from "./pages/BuySell/Buy/Buy";
+import Sell from "./pages/BuySell/Sell/Sell";
+import StylePage from "./pages/StylePage/StylePage";
 import Main from "./pages/Main/Main";
-// import MyPage from "./pages/MyPage/MyPage";
+import MyPage from "./pages/MyPage/MyPage";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      {/* <Nav /> */}
+      <Nav />
       <Routes>
         <Route path="/" element={<Main />} />
-        {/* <Route path="/product" element={<ProductList />} />
+        <Route path="/product" element={<ProductList />} />
         <Route path="/detail" element={<ProductDetail />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/buy" element={<Buy />} />
         <Route path="/sell" element={<Sell />} />
         <Route path="/style" element={<StylePage />} />
-        <Route path="/mypage" element={<MyPage />} /> */}
+        <Route path="/mypage" element={<MyPage />} />
       </Routes>
-      {/* <Footer /> */}
+      <Footer />
     </BrowserRouter>
   );
 };
--- src/style/GlobalStyle.js
+++ src/style/GlobalStyle.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap');
-  * {
-    box-sizing: border-box;
-    font-family: "Noto Sans KR", sans-serif;
+${reset}  
+* {
+    box-sizing: border-box; 
   }
+
+  body{
+      font-family: "Noto Sans KR", sans-serif;   
+    }
 `;
 export default GlobalStyle;
