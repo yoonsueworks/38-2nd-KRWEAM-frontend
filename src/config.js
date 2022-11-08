86d61ffcee9048fd40647d6d2fc48863604dc359
HolyWatter <qktjdtn97@icloud.com>
2022-11-08 14:43:11 +0900

    config파일에 api주소 추가

--- src/config.js
+++ src/config.js
@@ -1,2 +1,13 @@
 export const APIKEY = process.env.REACT_APP_API_KEY;
 export const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
+
+const BASE_URL_TS = "http://10.58.52.214:3000";
+const BASE_URL_CW = "http://10.58.52.112:3000";
+
+export const api = {
+  wish: `${BASE_URL_TS}/wish`,
+  products: `${BASE_URL_CW}/products`,
+  dealhistories: `${BASE_URL_TS}/dealhistory`,
+  deal: `${BASE_URL_TS}/deal`,
+  detail: `${BASE_URL_TS}/detail`,
+};
